e3260628deaa6475deb4baecb9758e5a65610e75
joe <zhangyj@3gmac.com>
2015-03-15 03:11:05 +0800

    propertyvaluesholder

--- src/com/joez/animation/MainActivity.java
+++ src/com/joez/animation/MainActivity.java
@@ -1,6 +1,9 @@
 package com.joez.animation;
 
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -72,13 +75,23 @@ public class MainActivity extends ActionBarActivity {
 				alphChange(mIvTarget);
 				break;
 			case R.id.btn_scale:
+				scaleChange(mIvTarget);
 				break;
 			case R.id.btn_mix:
+				mixChange(mIvTarget);
 				break;
 			default:
 				break;
 			}
 			
+		}
+		
+		private void mixChange(View view){
+			int left=view.getLeft();
+			PropertyValuesHolder scaleXHolder=PropertyValuesHolder.ofFloat("translationX", left,mScreenWidth-view.getRight()-view.getWidth(),left);
+//			PropertyValuesHolder scaleYHolder=PropertyValuesHolder.ofFloat("scaleY", 1,2,1);
+			
+			ObjectAnimator.ofPropertyValuesHolder(view, scaleXHolder).setDuration(5000).start();
 		}
 		
 		private void transLeftToRight(View view){
@@ -88,7 +101,7 @@ public class MainActivity extends ActionBarActivity {
 			animator.setDuration(3000).start();
 		}
 		private void rotation(View view){
-			ObjectAnimator animator=ObjectAnimator.ofFloat(view, "rotation", 0,360);
+			ObjectAnimator animator=ObjectAnimator.ofFloat(view, "rotationX", 0,360);
 			animator.setInterpolator(new CycleInterpolator(100));
 			animator.setDuration(30000).start();;
 		}
@@ -98,6 +111,22 @@ public class MainActivity extends ActionBarActivity {
 			animator.setInterpolator(new LinearInterpolator());
 			animator.setDuration(10000).start();
 		}
+		
+		private void scaleChange(final View view){
+			ObjectAnimator animator=ObjectAnimator.ofFloat(view, "xxx", 2);
+			animator.setInterpolator(new LinearInterpolator());
+			animator.setDuration(3000);
+			animator.addUpdateListener(new AnimatorUpdateListener() {
+				
+				@Override
+				public void onAnimationUpdate(ValueAnimator animation) {
+					float value=(Float) animation.getAnimatedValue();
+					view.setScaleX(value);
+					view.setScaleY(value);
+				}
+			});
+			animator.start();
+		}
+		
 	}
-
 }
